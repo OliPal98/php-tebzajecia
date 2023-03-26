3fe77c428e7555f8f2c6d6456051b30a53595145
OliPal98 <tel.kindly98@gmail.com>
2023-03-26 16:27:49 +0200

    Add controller

--- index.php
+++ index.php
@@ -4,35 +4,13 @@ declare(strict_types=1);
 
 namespace App;
 
-include_once('./SRC/view.php');
+include_once('./SRC/controller.php');
 include_once('./SRC/utils/debug.php');
 
-const DEFAULT_ACTION = 'list';
-
-$action = $_GET['action'] ?? DEFAULT_ACTION;
-
-$viewParams = [];
-
-if ($action === 'create'){
-    $page = 'create';
-    $created = false;
-    if (!empty($_POST)){
-        $viewParams = [
-            'title'=>$_POST['title'],
-            'description'=>$POST['description'],
-        ];
-        $created = true;
-    }
-    $viewParams['created'] = $created;
-} else {
-    $page = 'list';
-    $viewParams['resultList'] = 'Wyświetlamy listę notatek';
-}
 
 
 
-
-$view = new view();
-$view->render($page, $viewParams);
+$controller = new controller($_GET, $_POST);
+$controller->run();
 
 ?>

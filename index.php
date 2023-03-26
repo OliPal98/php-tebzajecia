d59497b3026c56b582478d54d65697273dc84608
OliPal98 <tel.kindly98@gmail.com>
2023-03-26 13:57:33 +0200

    modified

--- index.php
+++ index.php
@@ -7,10 +7,22 @@ namespace App;
 include_once('./SRC/view.php');
 include_once('./SRC/utils/debug.php');
 
-$action = $_GET['action'] ?? null;
+const DEFAULT_ACTION = 'list';
+
+$action = $_GET['action'] ?? DEFAULT_ACTION;
+
+$viewParams = [];
+
+if ($action === 'create'){
+    $viewParams['resultCreate'] = 'Tworzymy nową notatkę';
+} else {
+    $viewParams['resultList'] = 'Wyświetlamy listę notatek';
+}
 
 
 $view = new view();
-$view->render($action);
+
+
+$view->render($action, $viewParams);
 
 ?>

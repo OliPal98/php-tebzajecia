43375e080396d15c47a97033d708269d10ff62c6
OliPal98 <tel.kindly98@gmail.com>
2023-04-16 16:20:39 +0200

    Add basic AppException

--- index.php
+++ index.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace App;
 
+require_once('./Exception/AppException.php');
 include_once('./SRC/controller.php');
 include_once('./SRC/utils/debug.php');
 require_once('./config/config.php');
 
 
 
+try {
 Controller::initConfiguration($configuration);
 $controller = new controller($_GET, $_POST);
 $controller->run();
+} catch (AppException $e) {
+    echo "<h1>Wystąpił błąd w aplikacji</h1>";
+    echo '<h3>' . $e->getMessage() . '</h3>';
+}
 
 ?>

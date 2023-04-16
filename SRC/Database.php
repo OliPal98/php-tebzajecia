213e1e438fbd2350e2a2bf6b471abae4cb39c238
OliPal98 <tel.kindly98@gmail.com>
2023-04-16 22:00:47 +0200

    add basic appException

--- SRC/Database.php
+++ SRC/Database.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace App;
 
+use Exception;
 use PDO;
+use PDOException;
 
 class database
 {
     public function __construct(array $config)
     {
+        try {
         $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
         $connection = new PDO('ssssss');
+    } catch (PDOException $e) {
+        throw new AppException('Błąd w połączeniu z bazą danych');
     }
+}
 }

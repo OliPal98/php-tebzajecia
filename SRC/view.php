4f8d5b27802f1280537adf2046eee63b75a36771
OliPal98 <tel.kindly98@gmail.com>
2023-05-06 13:34:39 +0200

    Controller Config

--- SRC/view.php
+++ SRC/view.php
@@ -6,7 +6,7 @@ namespace App;
 
 class view
 {
-    public function render(string $page, array $params): void
+    public function render(string $page, array $params = []): void
     {
         require_once('./templates/layout.php');
     }

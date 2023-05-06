7f49c732fb41f3d0af77cf24572a2adb42effa58
OliPal98 <tel.kindly98@gmail.com>
2023-05-06 19:04:31 +0200

    add Edit

--- SRC/Controller/NoteController.php
+++ SRC/Controller/NoteController.php
@@ -16,8 +16,7 @@ class NoteController extends AbstractController
                 'description' => $this->request->postParam('description'),
             ];
             $this->database->createNote($noteData);
-            header('Location: /?before=created');
-            exit;
+            $this->redirect('/', ['before'=> 'created']);
         }
         $this->view->render('create');
     }
@@ -26,13 +25,12 @@ class NoteController extends AbstractController
     {
         $noteId = (int) $this->request->getParam('id') ?? null;
         if (!$noteId) {
-            header('Location: /?error=missingNoteId');
-            exit;
+            $this->redirect('/', ['error' => 'missingNoteId']);
         }
         try {
             $note = $this->database->getNote($noteId);
         } catch (NotFoundException $e){
-            header('Location: /?error=noteNotFound');
+            $this->redirect('/', ['error' => 'noteNotFound']);
         }
         $this->view->render('show', ['note'=> $note]);
     }
@@ -44,5 +42,14 @@ class NoteController extends AbstractController
             'before' => $this->request->getParam('before') ?? null,
             'error' => $this->request->getParam('error') ?? null,
         ]);
+    }
+
+    public function editAction()
+    {
+        $noteId = (int) $this->request->getParam('id');
+        if (!$noteId) {
+            $this->redirect('/', ['error' => 'missingNoteId']);
+        }
+        $this->view->render('edit');
     }
 }
---
+++ templates/pages/edit.php
@@ -0,0 +1,29 @@
+<h3>Edycja notatki</h3>
+<div>
+    <?php if (!empty($params['note'])) : ?>
+        <?php
+        $note = $params['note'];
+        dump($params); ?>
+        <form action="/?action=edit" class="note-form" method="post">
+            <input type="text" name="id" value="<?php echo $note['id']> ?>" />
+            <ul>
+                <li>
+                    <label for="title">Tytuł<span class="required">*</span></label>
+                    <input value="<?php echo $note['title'] ?>" type="text" name="title" id="title" class="filed-long">
+                </li>
+                <li>
+                    <label for="field5">Treść</label>
+                    <textarea name="description" id="field5" class="field-long field-textarea"><?php echo $note['description'] ?></textarea>
+                </li>
+                <li>
+                    <input type="submit" value="Submit">
+                </li>
+            </ul>
+    </form>
+    <?php else : ?>
+        <div>
+            Brak danych do wyświetlenia
+            <a href="/"><button>Powrót do listy notatek</button></a>
+        </div>
+        <?php endif; ?>
+</div>
